04cbaed6e605a2296e7dbb964e1b1a3525ae4cd0
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-07-23 23:17:12 +0900

    Add E2E test of user signup feature (#25)

---
+++ spec/system/users/signup_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'UsersSignup', type: :system do
+  before { visit new_user_path }
+
+  context 'when user information is valid' do
+    it 'signup a user' do
+      fill_in 'ユーザー名（50文字以内）', with: 'test'
+      fill_in 'メールアドレス', with: 'test@gmail.com'
+      fill_in 'パスワード（6文字以上）', with: 'password'
+      fill_in '確認用パスワード', with: 'password'
+      expect { click_button '登録する' }.to change(User, :count).by(1)
+    end
+  end
+
+  context 'when user information is invalid' do
+    it 'do not signup a user' do
+      fill_in 'ユーザー名（50文字以内）', with: ''
+      fill_in 'メールアドレス', with: 'testgmail.com'
+      fill_in 'パスワード（6文字以上）', with: 'password'
+      fill_in '確認用パスワード', with: 'pasword'
+      expect { click_button '登録する' }.to change(User, :count).by(0)
+      expect(page).to have_css '.error-messages'
+    end
+  end
+end
