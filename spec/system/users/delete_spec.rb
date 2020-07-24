b599463124556a77a28495f6ba8b256eb99be3f2
shohei-12 <shoheimaeda10@gmail.com>
2020-07-24 14:59:05 +0900

    Add E2E test of user delete feature

---
+++ spec/system/users/delete_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'UsersDelete', js: true, type: :system do
+  before do
+    @test1 = create(:test1)
+    visit users_path
+  end
+
+  context 'when accept the confirmation dialog' do
+    before { accept_confirm { click_link '削除する' } }
+
+    it 'delete a user' do
+      expect(page).to have_current_path users_path
+      expect(User.count).to eq 0
+    end
+  end
+
+  context 'when dismiss the confirmation dialog' do
+    before { dismiss_confirm { click_link '削除する' } }
+
+    it 'do not delete a user' do
+      expect(page).to have_current_path users_path
+      expect(User.count).to eq 1
+    end
+  end
+end
