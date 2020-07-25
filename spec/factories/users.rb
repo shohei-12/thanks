d29fde4e2625652ace757facc8b4c86fb942b447
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-07-25 22:36:44 +0900

    Add E2E test of login feature (#44)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,11 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
   end
+
+  factory :guest, class: User do
+    name { 'guest' }
+    email { 'guest@gmail.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
 end
