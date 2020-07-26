2c36fcb5a8da81ef86aff5541d79f8f622e7fa37
shohei-12 <shoheimaeda10@gmail.com>
2020-07-26 20:04:57 +0900

    Add post list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/guest_login', to: 'sessions#guest'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :posts
+  resources :posts, except: %i[index]
 end
