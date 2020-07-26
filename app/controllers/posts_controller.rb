8bb30fd413ee9e9dcbc808106718b24df0c16710
shohei-12 <shoheimaeda10@gmail.com>
2020-07-26 20:17:35 +0900

    Add post detail page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,10 @@ class PostsController < ApplicationController
     @posts = Post.all.order(created_at: :desc)
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
