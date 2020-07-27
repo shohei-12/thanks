6569473e14d7a2c87a0800bb85a2b2c162ac885b
shohei-12 <shoheimaeda10@gmail.com>
2020-07-27 13:43:33 +0900

    Update current_user helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,12 +12,12 @@ module SessionsHelper
   # Return the logged in user
   def current_user
     if (user_id = session[:user_id])
-      User.find_by(id: user_id)
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
         log_in user
-        user
+        @current_user = user
       end
     end
   end
