a565c6b23e3ea1fc606f9d0a3a721724b7513042
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-07-27 21:23:18 +0900

    Cannot edit and update and delete other users posts (#62)
    * Cannot edit and update and delete other users posts
    
    * Fix before_action of posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :check_login, only: %i[new create show edit update destroy]
+  before_action :check_user, only: %i[edit update destroy]
+
   def new
     @post = current_user.posts.build
   end
@@ -20,12 +23,9 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post
     else
@@ -34,7 +34,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path
   end
@@ -46,5 +45,13 @@ class PostsController < ApplicationController
       :title,
       :content
     )
+  end
+
+  # before action
+
+  # Check user is self
+  def check_user
+    @post = Post.find(params[:id])
+    redirect_to root_path unless current_user?(@post.user)
   end
 end
