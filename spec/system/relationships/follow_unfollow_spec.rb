157fef848f261bcb933ecd7772618f65593fee29
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-07-28 23:25:09 +0900

    Add E2E test of follow feature (#69)

---
+++ spec/system/relationships/follow_unfollow_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'RelationshipsFollowUnfollow', js: true, type: :system do
+  before do
+    @test1 = create(:test1)
+    @test2 = create(:test2)
+    log_in @test1
+    visit user_path(@test2)
+  end
+
+  it 'can follow and unfollow other user' do
+    click_button 'フォローする'
+    expect(page).to have_button 'フォローを解除する'
+    relationship = Relationship.first
+    expect(relationship.user_id).to eq @test1.id
+    expect(relationship.follow_id).to eq @test2.id
+    click_button 'フォローを解除する'
+    expect(page).to have_button 'フォローする'
+    expect(Relationship.count).to eq 0
+  end
+end
