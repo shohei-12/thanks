2fa863c1b0859424a4f3885977a0c7ef6faeb8da
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-07-29 20:34:34 +0900

    Add comment delete feature (#73)
    * Add comment delete feature
    
    * Disable the deletion of other people's comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :check_login
+  before_action :check_commenter, only: %i[destroy]
 
   def create
     @post = Post.find(params[:comment][:post_id])
@@ -8,11 +9,23 @@ class CommentsController < ApplicationController
     @comment.save
   end
 
+  def destroy
+    @comment.destroy
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(
       :content
     )
+  end
+
+  # before action
+
+  # Check commenter is self
+  def check_commenter
+    @comment = Comment.find(params[:id])
+    redirect_to root_path unless current_user?(@comment.user)
   end
 end
