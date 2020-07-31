2e1df0429bbd81e045380dc9108f8bb435482d3b
shohei-12 <shoheimaeda10@gmail.com>
2020-07-31 22:56:31 +0900

    Fix RSpec

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :category do
-    
+  factory :category1, class: Category do
+    name { 'お父さん・お母さん' }
   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :post1, class: Post do
     association :user, factory: :test1
+    association :category, factory: :category1
     sequence(:title) { |i| "test#{i}" }
     sequence(:content) { |i| "test#{i}です。" }
   end
