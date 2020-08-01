f925f472e76972bca8ff801ebc63fe8171132092
shohei-12 <shoheimaeda10@gmail.com>
2020-08-01 20:34:02 +0900

    Add unit test of comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:comment1) { build(:comment1) }
+
+  describe 'comment1' do
+    context 'when comment1 is valid' do
+      it 'return true' do
+        expect(comment1.valid?).to eq true
+      end
+    end
+  end
+
+  describe 'content' do
+    context 'when content is invalid' do
+      context 'when content is empty' do
+        before { comment1.content = '' }
+
+        it 'return false' do
+          expect(comment1.valid?).to eq false
+        end
+      end
+
+      context 'when content is over 400 characters' do
+        before { comment1.content = 'a' * 401 }
+
+        it 'return false' do
+          expect(comment1.valid?).to eq false
+        end
+      end
+    end
+  end
 end
