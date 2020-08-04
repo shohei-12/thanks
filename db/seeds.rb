7713360359f89b91068e35275ffebe234d783829
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-08-04 14:17:17 +0900

    Create initial data (#104)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,45 @@
+# guest user
+User.create!(
+  name: 'ゲストユーザー',
+  email: 'guest@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+# user
+50.times do |n|
+  User.create!(
+    name: Gimei.unique.name.kanji,
+    email: "example#{n + 1}@example.com",
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
 # category
+categories = %w[お父さん・お母さん お兄ちゃん・お姉ちゃん 弟・妹 おじいちゃん・おばあちゃん 旦那さん・奥さん 息子・娘 孫 彼氏・彼女 友達 その他]
 
-Category.create!(
-  name: 'お父さん・お母さん'
-)
+categories.each do |category|
+  Category.create!(
+    name: category
+  )
+end
 
-Category.create!(
-  name: 'お兄ちゃん・お姉ちゃん'
-)
+# post
+30.times do |n|
+  Post.create!(
+    title: "テスト#{n + 1}",
+    content: "テスト#{n + 1}です。",
+    user_id: 1,
+    category_id: 1
+  )
+end
 
-Category.create!(
-  name: '弟・妹'
-)
-
-Category.create!(
-  name: 'おじいちゃん・おばあちゃん'
-)
-
-Category.create!(
-  name: '旦那さん・奥さん'
-)
-
-Category.create!(
-  name: '息子・娘'
-)
-
-Category.create!(
-  name: '孫'
-)
-
-Category.create!(
-  name: '彼氏・彼女'
-)
-
-Category.create!(
-  name: '友達'
-)
-
-Category.create!(
-  name: 'その他'
-)
+# comment
+30.times do |n|
+  Comment.create!(
+    content: "テストコメント#{n + 1}です。",
+    user_id: rand(1..51),
+    post_id: 30
+  )
+end
