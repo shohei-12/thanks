15680ebf3ff667566b10b4d2004d0332eb1603f0
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-08-09 21:29:08 +0900

    [remove] Stop using materialize (#125)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,24 +8,9 @@ require("@rails/activestorage").start();
 require("channels");
 require("jquery");
 
-import "../stylesheets/application";
-import "materialize-css/dist/js/materialize";
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-$(function () {
-  // header
-  $(".dropdown-trigger").dropdown();
-
-  // hamburger menu
-  $(".sidenav").sidenav({
-    edge: "right",
-  });
-  $(".character-counter").characterCounter();
-  $("select").formSelect();
-});
