adae5ada5cf69ae2079b30ed46597e249c8f79d7
shohei-12 <shoheimaeda10@gmail.com>
2020-08-10 17:42:44 +0900

    [design] Modify header and footer UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,16 @@ require("jquery");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(function () {
+  // hamburger menu
+  $(".hamburger").on("click", function () {
+    if ($(this).hasClass("open")) {
+      $(this).removeClass("open");
+      $("nav").removeClass("open");
+    } else {
+      $(this).addClass("open");
+      $("nav").addClass("open");
+    }
+  });
+});
