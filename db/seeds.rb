b6a8d0556dcf0f523ca3f515c95d28b80cfe01c7
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-08-11 22:40:49 +0900

    Design/modify post form ui (#135)
    * [refactor] Refactoring the user form and login form code
    
    * [design] Modify post form UI
    
    * [update] Update initial data
    
    * [remove] Remove the user form icon
    
    * [fix] Fix test

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,8 @@ end
     title: "テスト#{n + 1}",
     content: "テスト#{n + 1}です。",
     user_id: 1,
-    category_id: 1
+    category_id: 1,
+    check: rand(0..1)
   )
 end
 
--- spec/system/posts/update_spec.rb
+++ spec/system/posts/update_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'PostsUpdate', type: :system do
 
     context 'when post information is valid' do
       it 'update a post information' do
-        fill_in 'タイトル（50文字以内）', with: 'テスト'
-        fill_in '内容（400文字以内）', with: 'テストです。'
+        fill_in 'タイトル', with: 'テスト'
+        fill_in '内容', with: 'テストです。'
         click_button '更新する'
         @post1.reload
         expect(@post1.title).to eq 'テスト'
@@ -22,8 +22,8 @@ RSpec.describe 'PostsUpdate', type: :system do
 
     context 'when post information is invalid' do
       it 'do not update a post information' do
-        fill_in 'タイトル（50文字以内）', with: ''
-        fill_in '内容（400文字以内）', with: ''
+        fill_in 'タイトル', with: ''
+        fill_in '内容', with: ''
         click_button '更新する'
         @post1.reload
         expect(@post1.title).to eq @post1.title
