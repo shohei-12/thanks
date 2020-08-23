73c40b0b90490f6d654328a69dc804ad5e7c67cb
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-08-23 21:01:13 +0900

    [test] Review post system spec (#163)

--- spec/system/posts/delete_spec.rb
+++ spec/system/posts/delete_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'PostsDelete', js: true, type: :system do
   before { @post1 = create(:post1) }
 
   context 'when the user is logged in' do
-    context 'when view the details page of own post' do
+    context 'when access the details page of own post' do
       before do
         log_in(@post1.user)
         visit post_path(@post1)
@@ -14,7 +14,7 @@ RSpec.describe 'PostsDelete', js: true, type: :system do
         before { accept_confirm { click_link '削除する' } }
 
         it 'delete a post' do
-          expect(page).to have_current_path user_path(@post1.user)
+          expect(current_path).to eq user_path(@post1.user)
           expect(Post.count).to eq 0
         end
       end
@@ -23,28 +23,25 @@ RSpec.describe 'PostsDelete', js: true, type: :system do
         before { dismiss_confirm { click_link '削除する' } }
 
         it 'do not delete a post' do
-          expect(page).to have_current_path post_path(@post1)
+          expect(current_path).to eq post_path(@post1)
           expect(Post.count).to eq 1
         end
       end
     end
 
-    context 'when view the details page of others post' do
-      before do
+    context 'when access the details page of others post' do
+      it 'do not display delete link' do
         log_in(create(:test2))
         visit post_path(@post1)
-      end
-
-      it 'do not display delete link' do
-        expect(page).not_to have_link '削除する'
+        expect(page).not_to have_link '削除する', href: post_path(@post1)
       end
     end
   end
 
   context 'when the user is not logged in' do
-    it 'cannot delete a post' do
+    it 'cannot access post details page' do
       visit post_path(@post1)
-      expect(page).to have_current_path login_path
+      expect(current_path).to eq login_path
     end
   end
 end
--- spec/system/posts/update_spec.rb
+++ spec/system/posts/update_spec.rb
@@ -1,34 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe 'PostsUpdate', type: :system do
+  let(:test2) { create(:test2) }
+
   before { @post1 = create(:post1) }
 
   context 'when the user is logged in' do
-    before do
-      log_in(@post1.user)
-      visit edit_post_path(@post1)
-    end
+    context 'when access the edit page of own post' do
+      before do
+        log_in(@post1.user)
+        visit post_path(@post1)
+        click_link '編集する'
+      end
 
-    context 'when post information is valid' do
-      it 'update a post information' do
-        fill_in 'タイトル', with: 'テスト'
-        fill_in '内容', with: 'テストです。'
-        click_button '更新する'
-        @post1.reload
-        expect(@post1.title).to eq 'テスト'
-        expect(@post1.content).to eq 'テストです。'
+      context 'when post information is valid' do
+        it 'update a post information' do
+          fill_in 'タイトル', with: 'テスト'
+          fill_in '内容', with: 'テストです。'
+          click_button '更新する'
+          @post1.reload
+          expect(@post1.title).to eq 'テスト'
+          expect(@post1.content).to eq 'テストです。'
+        end
+      end
+
+      context 'when post information is invalid' do
+        it 'do not update a post information' do
+          fill_in 'タイトル', with: 'a' * 51
+          fill_in '内容', with: 'a' * 401
+          click_button '更新する'
+          expect(page).to have_css '.error-message'
+          @post1.reload
+          expect(@post1.title).to eq @post1.title
+          expect(@post1.content).to eq @post1.content
+        end
       end
     end
 
-    context 'when post information is invalid' do
-      it 'do not update a post information' do
-        fill_in 'タイトル', with: ''
-        fill_in '内容', with: ''
-        click_button '更新する'
-        @post1.reload
-        expect(@post1.title).to eq @post1.title
-        expect(@post1.content).to eq @post1.content
-        expect(page).to have_css '.error-message'
+    context 'when access the details page of others post' do
+      it 'do not display edit link' do
+        log_in(test2)
+        visit post_path(@post1)
+        expect(page).not_to have_link '編集する', href: edit_post_path(@post1)
+      end
+    end
+
+    context 'when access the edit page of others post' do
+      it 'redirect to the top page' do
+        log_in(test2)
+        visit edit_post_path(@post1)
+        expect(current_path).to eq root_path
       end
     end
   end
@@ -36,7 +57,7 @@ RSpec.describe 'PostsUpdate', type: :system do
   context 'when the user is not logged in' do
     it 'cannot access post edit page' do
       visit edit_post_path(@post1)
-      expect(page).to have_current_path login_path
+      expect(current_path).to eq login_path
     end
   end
 end
