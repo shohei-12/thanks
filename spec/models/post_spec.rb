1a28af9156a5c7507a77e145343fe6547f64c452
shohei-12 <shoheimaeda10@gmail.com>
2020-08-24 13:20:53 +0900

    [test] Review post and comment model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,14 +3,6 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   let(:post1) { build(:post1) }
 
-  describe 'post1' do
-    context 'when post1 is a valid post' do
-      it 'return true' do
-        expect(post1.valid?).to eq true
-      end
-    end
-  end
-
   describe 'title' do
     context 'when title is invalid' do
       context 'when title is empty' do
@@ -47,6 +39,42 @@ RSpec.describe Post, type: :model do
         it 'return false' do
           expect(post1.valid?).to eq false
         end
+      end
+    end
+  end
+
+  describe 'check' do
+    context 'when check is invalid' do
+      context 'when check is empty' do
+        before { post1.check = '' }
+
+        it 'return false' do
+          expect(post1.valid?).to eq false
+        end
+      end
+
+      context 'when check has an unexpected value' do
+        before { post1.check = 2 }
+
+        it 'return false' do
+          expect(post1.valid?).to eq false
+        end
+      end
+    end
+  end
+
+  describe '#self.search(keyword)' do
+    before { @post1 = create(:post1) }
+
+    context 'when posts containing keyword is exist' do
+      it 'get posts containing keyword' do
+        expect(Post.search('test').include?(@post1)).to eq true
+      end
+    end
+
+    context 'when posts containing keyword is not exist' do
+      it 'not get posts' do
+        expect(Post.search('hoge').length).to eq 0
       end
     end
   end
