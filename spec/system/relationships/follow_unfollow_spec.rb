00bb5ca9b7c0e5e7df7fce2f650d1f0b28922f22
shohei-12 <shoheimaeda10@gmail.com>
2020-09-01 15:42:17 +0900

    [fix] Fix test

--- spec/system/relationships/follow_unfollow_spec.rb
+++ spec/system/relationships/follow_unfollow_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe 'FollowUnfollow', js: true, type: :system do
     log_in test1
     visit user_path(test2)
     click_button 'フォロー'
-    expect(page).to have_button 'フォロー中'
+    expect(page).to have_button 'フォロー解除'
     relationship = Relationship.first
     expect(relationship.user_id).to eq test1.id
     expect(relationship.follow_id).to eq test2.id
-    click_button 'フォロー中'
+    click_button 'フォロー解除'
     expect(page).to have_button 'フォロー'
     expect(Relationship.count).to eq 0
   end
