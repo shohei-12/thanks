274c2fe0e37540b2db6ef2b78b6baaf4148b1e21
shohei-12 <shoheimaeda10@gmail.com>
2020-09-09 10:43:01 +0900

    [design] Modify post details page UI

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,5 +1,5 @@
 $(document).on("turbolinks:load", function () {
   if (!$(".post-details .comments .comment").length) {
-    $(".post-details .comment-list").hide();
+    $(".post-details .comment-list, .post-details .comment-sort").hide();
   }
 });
